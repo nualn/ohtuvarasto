403a96c7655b100428368027f2a884ca740549df
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2022-11-11 00:31:53 +0200

    fix everything

--- src/varasto.py
+++ src/varasto.py
@@ -1,34 +1,10 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
-            if True:
-                self.saldo = tilavuus
-                if True:
-                    self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
-            self.saldo = tilavuus
+        self.tilavuus = max(tilavuus,0.0)
+        self.saldo = max(min(alku_saldo, tilavuus), 0)
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
